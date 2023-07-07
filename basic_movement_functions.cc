c68efdf348f482e1c6a35907c7af39c3b96708f3
Zitian Qin <84650698+Sitiam@users.noreply.github.com>
2023-07-07 23:10:38 +1000

    Update basic_movement_functions.cc

--- basic_movement_functions.cc
+++ basic_movement_functions.cc
@@ -33,17 +33,16 @@ int inB1 = 5;
 int inB2 = 4;
 
 
-struct {
+struct motor {
 	int en;
 	int in1;
 	int in2;
 	int max_speed;
 	int min_speed;
-} motorA, MotorB;
+};
 
-
-void setup() {
-	// Set all the motor control pins to outputs using pinMode()
+// Set all the motor control pins to outputs using pinMode()
+void pinSetup() {
 	pinMode(enA, OUTPUT);
 	pinMode(enB, OUTPUT);
 	pinMode(inA1, OUTPUT);
@@ -52,30 +51,38 @@ void setup() {
 	pinMode(inB2, OUTPUT);
 }
 
+void setup() {
+	pinSetup();
+}
+
 // Speed between -MAX_SPEED (negative MAX_SPEED) and +MAX_SPEED (positive MAX_SPEED)
 // Negative speed makes the motor spin backwards
-driveSingleWheel(int motor_in_1, int motor_in_2, int en_motor, int speed) {
-	digitalWrite(inA_1, HIGH);
-	digitalWrite(inA_2, LOW);
+driveSingleWheel(motor selected_motor, int speed) {
+	if (speed < 0) {
+		digitalWrite(selected_motor.in1, LOW);
+		digitalWrite(selected_motor.in2, HIGH);
+		analogWrite(selector_motor.en, speed);
+	} else {
+		digitalWrite(selected_motor.in1, HIGH);
+		digitalWrite(selected_motor.in2, LOW);
+		analogWrite(selector_motor.en, speed);
+	}
 }
 
-void driveForward(int speed) {
-	digitalWrite(inA_1, HIGH);
-	digitalWrite(inA_2, LOW);
-	digitalWrite(inB_1, HIGH);
-	digitalWrite(inB_1, LOW);
-	analogWrite(enA, speed);
-	analogWrite(enB, speed);
+// Negative speed = backwards
+void driveStraight(motor motorA, motor motorB, int speed) {
+	driveSingleWheel(motorA, speed);
+	driveSingleWheel(motorB, speed);
 }
 
-void driveBackward(int speed) {
-	digitalWrite(inA_1, LOW);
-	digitalWrite(inA_2, HIGH);
-	digitalWrite(inB_1, LOW);
-	digitalWrite(inB_1, HIGH);
-	analogWrite(enA, speed);  // Idk if this line is needed cos example code in slides doesn't have this
-	analogWrite(enB, speed);  // Idk if this line is needed cos example code in slides doesn't have this
-}
+// void driveBackward(int speed) {
+// 	digitalWrite(inA_1, LOW);
+// 	digitalWrite(inA_2, HIGH);
+// 	digitalWrite(inB_1, LOW);
+// 	digitalWrite(inB_1, HIGH);
+// 	analogWrite(enA, speed);  // Idk if this line is needed cos example code in slides doesn't have this
+// 	analogWrite(enB, speed);  // Idk if this line is needed cos example code in slides doesn't have this
+// }
 
 void turnOffMotors() {
 	digitalWrite(inA_1, LOW);
@@ -89,19 +96,12 @@ void turnOffMotors() {
 // turn_speed should be an integer value between 0 and 100. 0 turn speed will not make the robot turn. 100 turn speed will make the robot turn very quickly on the spot.
 // turn_speed in between will make it turn at a slower pace by making the robot still move forward as it turns. 
 void forward_turn(int turn_direction, int turn_speed) {
-	// if (turn_speed == 0) {
-	// 	return
-	// }
-	if (turn_direction == -1) {
-		digitalWrite(inA_1, HIGH);
-		digitalWrite(inA_2, LOW);
-		digitalWrite(inB_1, HIGH);
-		digitalWrite(inB_1, LOW);
-	} (turn_direction == 0) {}
-	digitalWrite(inA_1, HIGH);
-	digitalWrite(inA_2, LOW);
-	digitalWrite(inB_1, HIGH);
-	digitalWrite(inB_1, LOW);
+	// // if (turn_speed == 0) {
+	// // 	return
+	// // }
+	// if (turn_direction == -1) {
+	// } (turn_direction == 0) {}
+
 	
 }
 
@@ -112,6 +112,8 @@ void main() {
 	setup();
 	
 	// Set up the two motor structs
+	motor motorA;
+	motor motorB;
 	motorA.en = enA;
 	motorA.in1 = inA1;
 	motorA.in2 = inA2;
