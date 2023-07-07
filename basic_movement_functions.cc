d59119942c0963034d81a9c8fdc1e5642e720303
Zitian Qin <84650698+Sitiam@users.noreply.github.com>
2023-07-07 21:51:18 +1000

    Update basic_movement_functions.cc

--- basic_movement_functions.cc
+++ basic_movement_functions.cc
@@ -6,6 +6,12 @@
 MAX_SPEED = 255;
 MIN_SPEED = 0;
 
+// Directions
+ANTICLOCKWISE = -1;
+FORWARD = 0;
+CLOCKWISE = 1;
+
+// WE WILL HAVE TO CHANGE THESE VALUES FOR OUR ROBOT.
 // Motor A
 int enA = 9;
 int inA_1 = 8;
@@ -35,93 +41,20 @@ void driveForward(int speed) {
 	analogWrite(enB, speed);
 }
 
-void demoOne()
-{
-    // This function will run the motors in both directions at a fixed speed
-
-    // Turn on motor A -- digitalWrite() sets a pin to either HIGH (5V) or LOW (0V)
-    digitalWrite(in1, HIGH);
-    digitalWrite(in2, LOW);
-
-    // Set speed to 200 out of possible range 0~255 -- analogWrite() sends a PWM signal to the pin
-    analogWrite(enA, 200);
-
-    // Turn on motor B
-    digitalWrite(in3, HIGH);
-    digitalWrite(in4, LOW);
-
-    // Set speed to 200 out of possible range 0~255
-    analogWrite(enB, 200);
-    // pauses the program for a given amount of time - 2s
-    delay(2000);
-
-    // Now change motor directions -- reversing the motors
-    digitalWrite(in1, LOW);
-    digitalWrite(in2, HIGH);
-    digitalWrite(in3, LOW);
-    digitalWrite(in4, HIGH);
-
-    delay(2000);
-
-    // Now turn off motors
-    digitalWrite(in1, LOW);
-    digitalWrite(in2, LOW);
-    digitalWrite(in3, LOW);
-    digitalWrite(in4, LOW);
-
+void driveBackward(int speed) {
+	digitalWrite(inA_1, LOW);
+	digitalWrite(inA_2, HIGH);
+	digitalWrite(inB_1, LOW);
+	digitalWrite(inB_1, HIGH);
+	analogWrite(enA, speed);  // Idk if this line is needed cos example code in slides doesn't have this
+	analogWrite(enB, speed);  // Idk if this line is needed cos example code in slides doesn't have this
 }
 
-void demoTwo()
-{
-
-    // This function will run the motors across the range of possible speeds
-    // Note that maximum speed is determined by the motor itself and the operating voltage
-
-    // Turn on motors
-    digitalWrite(in1, LOW);
-    digitalWrite(in2, HIGH);
-    digitalWrite(in3, LOW);
-    digitalWrite(in4, HIGH);
-
-    // Accelerate from zero to maximum speed
-    for (int i = 0; i < 256; i++)
-    {
-
-        analogWrite(enA, i);
-        analogWrite(enB, i);
-
-        delay(20);
-
-    }
-
-    // Decelerate from maximum speed to zero
-    for (int i = 255; i >= 0; --i)
-    {
-
-        analogWrite(enA, i);
-        analogWrite(enB, i);
-
-        delay(20);
-
-    }
-
-    // Now turn off motors
-    digitalWrite(in1, LOW);
-    digitalWrite(in2, LOW);
-    digitalWrite(in3, LOW);
-    digitalWrite(in4, LOW);
-
+void turnOffMotors() {
+	digitalWrite(inA_1, LOW);
+	digitalWrite(inA_2, LOW);
+	digitalWrite(inB_1, LOW);
+	digitalWrite(inB_1, LOW);
 }
 
-void loop()
-{
-
-    demoOne();
-
-    delay(1000);
-
-    demoTwo();
-
-    delay(1000);
-
-}
+void turn()
